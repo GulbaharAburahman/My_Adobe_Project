6cb9e34536e06c58a456a96470139f0d9ac34ef9
Gulbahar <martinguli28@yahoo.co.nz>
2023-12-11 02:24:13 +1300

    Backend customer page fuctions and test runner

--- src/main/java/com/unitedcoder/Main.java
+++ src/main/java/com/unitedcoder/Main.java
@@ -1,17 +1,34 @@
 package com.unitedcoder;
 import com.unitedcoder.backend.LoginToAdminPage;
+import com.unitedcoder.backend.customerModule.CustomerPage;
 import com.unitedcoder.commonuse.BaseClass;
 import com.unitedcoder.commonuse.BrowserType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
      BaseClass baseClass=new BaseClass();
        baseClass.launchBrowser(BrowserType.CHROME);
         baseClass.navigateToBackEnd();
         LoginToAdminPage loginPage=new LoginToAdminPage(baseClass.driver);
         loginPage.loginToAdminPanelWithCredentials("CustomerManager");
+        CustomerPage customerPage=new CustomerPage(baseClass.driver);
+
+        System.out.println( customerPage.isCustomerAdded() );
+
+
+
+
+
+
 
 
     }
+
+
+
 }
---
+++ src/test/java/backendtests/AddCustomerTest.java
@@ -0,0 +1,41 @@
+package backendtests;
+
+import com.github.javafaker.Faker;
+import com.unitedcoder.backend.LoginToAdminPage;
+import com.unitedcoder.backend.customerModule.CustomerPage;
+import com.unitedcoder.commonuse.BaseClass;
+import com.unitedcoder.commonuse.BrowserType;
+import com.unitedcoder.commonuse.FunctionLibrary;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class AddCustomerTest extends BaseClass {
+    CustomerPage customerPage;
+    @BeforeClass
+    public void setup(){
+        launchBrowser(BrowserType.CHROME);
+         navigateToBackEnd();
+        LoginToAdminPage loginToAdminPage=new LoginToAdminPage(driver);
+        loginToAdminPage.loginToAdminPanelWithCredentials("customerManager");
+        customerPage=new CustomerPage(driver);
+    }
+
+    public void addCustomer(){
+     String firstName=FunctionLibrary.getFakeFirstname();
+     String lastname=FunctionLibrary.getFakeLastname();
+     String password=FunctionLibrary.getPassword();
+     String email=FunctionLibrary.getFakeEmail();
+     String DOB= Faker.instance().date().birthday().toString();
+     String middleName=Faker.instance().name().nameWithMiddle();
+        customerPage.addCustomer("Admin","Senem",firstName,middleName,lastname,email,DOB,"Female",
+                true,password);
+        Assert.assertTrue(customerPage.isCustomerAdded());
+    }
+
+    @AfterClass()
+    public void tearDown(){
+        teardown();
+    }
+
+}
